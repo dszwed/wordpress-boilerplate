6af62a3766545e01711289b6952aaf9bba1862ea
Dawid Szwed <dawid@gootek.pl>
2019-11-21 21:06:18 +0100

    Add unit tests

---
+++ wp-content/plugins/contact-form/contact-form.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Contact Form
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     contact-form
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Contact_Form
+ */
+
+// Your code starts here.
