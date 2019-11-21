72652790769494b56337dda2447e74f8863ab5be
Dawid Szwed <dawid@gootek.pl>
2019-11-21 22:13:02 +0100

    Add text area

--- wp-content/plugins/contact-form/templates/form.php
+++ wp-content/plugins/contact-form/templates/form.php
@@ -6,8 +6,8 @@
 		<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" name="email" required>
 	</div>
 	<div class="form-group">
-		<label for="exampleInputMessage">Message</label>
-		<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+		<label for="exampleFormControlTextarea1">Message</label>
+		<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="message"></textarea>
 	</div>
 	<button type="submit" class="btn btn-primary">Submit</button>
 </form>
